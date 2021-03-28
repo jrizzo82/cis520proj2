18def4b22dd71cc30d42afcb52be117f64d20cad
Connor Neil <cjneil@selene.beocat.ksu.edu>
2021-03-28 17:59:52 -0500

    changed makefile default to bochs

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -17,7 +17,6 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "userprog/syscall.h"
-#include "threads/malloc.h"	
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
